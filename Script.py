56ba299c2897ee5ee98909ad053ea799bc3d5c8a
Alfred Roos <alfred@stensatter.se>
2020-08-25 00:35:37 +0200

    branch test

--- Script.py
+++ Script.py
@@ -3,7 +3,7 @@ path= "C:/Users/Elev/Desktop/Projects/TaskOpeningScript/urls.txt"
 
 lines = open(path,"r")
 website = str(input())
-
+#branch s
 
 for line in lines:
 	splittedline = line.split(',')[0]
