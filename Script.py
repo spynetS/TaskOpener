87e25b23f97576223ef1c3b940396ce9aac2edf8
Alfred Roos <alfred@stensatter.se>
2020-08-25 15:41:57 +0200

    I made so the .loops. are in functions instead and everytime the function is done it starts it again, added allso help funcion

--- Script.py
+++ Script.py
@@ -1,14 +1,16 @@
-import webbrowser
+import webbrowser, sys
 path= "C:/Users/Elev/Desktop/Projects/TaskOpeningScript/urls.txt"
-
-lines = open(path,"r")
-website = input()
+run = True
 
 def NewSite():
         print("Write new url and shortcut")
         print("ex,www.example.com")
-        newsite=input()
-	
+        newsite=raw_input()
+        
+        if newsite == "CANCEL":
+                Loop()
+
+
         if "," in newsite:
                 print("you added "+newsite)
                 file = open(path,"a")
@@ -17,16 +19,31 @@ def NewSite():
         else:
                 print("Syntax error")
                 NewSite()
+def Loop():
+        print("MAIN")
+        print("Type HELP for help")
+        lines = open(path,"r")
+        website = raw_input()
+        
+        if website == "NEW":
+        	NewSite()
 
-if website == "NEW":
-	NewSite()
+        if website == "HELP":
+                print("SHOW shows you your urls")           
+                print("NEW takes you to the add window")           
+                print("EXIT closes the program")
+                print("CANCEL takes you back to the main window")           
+           
 
-for line in lines:
-	splittedline = line.split(',')[0]
-	
-	if splittedline==website: 
-		webbrowser.open(line.split(',')[1])
-	if 
-lines.close()
+        if website == "EXIT":
+                sys.exit("Exited program")
+        for line in lines:
+        	splittedline = line.split(',')[0]
+        	if splittedline==website: 
+        		webbrowser.open(line.split(',')[1])
+        		
+        lines.close()
+        Loop()
 
-input()
+if __name__ == '__main__':
+        Loop()
